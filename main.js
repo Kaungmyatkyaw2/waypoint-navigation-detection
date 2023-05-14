2fc78407c09ef43d2e4b50e6ab7fb4ffc2473a4e
sinwarnaw <sinwarnaw028@gmail.com>
2023-05-15 02:58:21 +0630

    errorNavActive

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import Typed from "typed.js";
 import ScrollReveal from "scrollreveal";
+import "waypoints/lib/noframework.waypoints";
 
 const typed = new Typed("#element", {
   strings: ["Food ...", "Drink ..."],
@@ -10,11 +11,85 @@ const typed = new Typed("#element", {
   backSpeed: 50,
 });
 
-var slideUp = {
+let toDown = {
   distance: "50px",
   origin: "top",
-  interval: 500,
-  duration: 1000,
+  interval: 300,
+  duration: 800,
 };
+ScrollReveal().reveal(".to-down", toDown);
 
-ScrollReveal().reveal(".to-down", slideUp);
+let toRight = {
+  distance: "50px",
+  origin: "left",
+  interval: 300,
+  duration: 2000,
+};
+ScrollReveal().reveal(".toRight", toRight);
+
+let toLeft = {
+  distance: "50px",
+  origin: "right",
+  interval: 300,
+  duration: 800,
+};
+ScrollReveal().reveal(".toLeft", toLeft);
+
+// waypoints
+// new Waypoint({
+//   element: document.getElementById("about"),
+//   handler: function (direction) {
+//     // let old = document.querySelector("currentActive");
+//     // old.classList.remove("currentActive");
+//     let current = document.querySelector(`[href="#about"]`);
+//     if (direction === "down") {
+//       current.classList.add("currentActive");
+//     } else {
+//       current.classList.remove("currentActive");
+//     }
+//   },
+//   offset: "25%",
+// });
+
+// new Waypoint({
+//   element: document.getElementById("home"),
+//   handler: function (direction) {
+//     // let old = document.querySelector("currentActive");
+//     // old.classList.remove("currentActive");
+//     let current = document.querySelector(`[href="#home"]`);
+//     if (direction === "down") {
+//       current.classList.add("currentActive");
+//     }
+//   },
+//   offset: "50%",
+// });
+
+// let sections = document.querySelectorAll("section");
+// let navlinks = document.querySelectorAll("header nav a");
+
+// window.onscroll = () => {
+//   sections.forEach((sec) => {
+//     let top = window.screenY;
+//     let offset = sec.offsetTop - 150;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute("id");
+
+//     if (top >= offset && top < offset + height) {
+//       navlinks.forEach((links) => {
+//         links.classList.remove("currentActive");
+//         document
+//           .querySelectorAll(`header nav a [href* = '+ ${id} + ']`)
+//           .classList.add("currentActive");
+//       });
+//     }
+//   });
+// };
+
+let activepage = window.location.href;
+
+const navlinks = document.querySelectorAll("nav a").forEach((link) => {
+  if (link.href.includes(`${activepage}`)) {
+    // console.log(`${activepage}`);
+    link.classList.add("currentActive");
+  }
+});
